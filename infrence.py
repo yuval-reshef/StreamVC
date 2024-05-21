e2c237cf6ce7169f97aaa5fe1f6dea5c952f3955
yonicohen <yonathan.cohen1@mail.huji.ac.il>
2024-05-21 16:47:44 +0300

    Inference script.

--- infrence.py
+++ infrence.py
@@ -1,9 +1,57 @@
 import argparse
 
-parser = argparse.ArgumentParser(
-    prog='StreamVC Inference Script',
-    description='Inference script for StreamVC model, performs voice conversion on a single audio file')
+import safetensors as st
+import safetensors.torch
+import soundfile as sf
+import torch
+import torchaudio.functional as F
 
-# pasrser.add_argument(...)
+from streamvc import StreamVC
 
-args = parser.parse_args()
+SAMPLE_RATE = 16_000
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DTYPE = torch.float32
+
+
+@torch.no_grad()
+def main(args):
+    """Main function for StreamVC model inference."""
+    model = StreamVC().to(device=DEVICE, dtype=DTYPE).eval()
+
+    encoder_state_dict = st.torch.load_file(args.checkpoint, device=DEVICE)
+
+    model.load_state_dict(encoder_state_dict)
+
+    source_speech, orig_sr = sf.read(args.source_speech)
+    source_speech = torch.from_numpy(source_speech).to(device=DEVICE, dtype=DTYPE)
+    if orig_sr != SAMPLE_RATE:
+        source_speech = F.resample(source_speech, orig_sr, SAMPLE_RATE)
+
+    target_speech, orig_sr = sf.read(args.target_speech)
+    target_speech = torch.from_numpy(target_speech).to(device=DEVICE, dtype=DTYPE)
+    if orig_sr != SAMPLE_RATE:
+        target_speech = F.resample(target_speech, orig_sr, SAMPLE_RATE)
+
+    output = model(source_speech, target_speech)
+    sf.write(args.output_path, output, SAMPLE_RATE)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='StreamVC Inference Script',
+        description='Inference script for StreamVC model, performs voice conversion on a single audio file')
+    parser.add_argument("--betas", type=float, nargs=2, default=(0.5, 0.9))
+
+    parser.add_argument("-c", "--checkpoint", type=str,
+                        default="./model.safetensors",
+                        help="A path the a pretrained StreamVC model checkpoint.")
+
+    parser.add_argument("-s", "--source-speech", type=str,
+                        help="A path to a an audio file with the source speech input for the model.")
+    parser.add_argument("-t", "--target-speech", type=str,
+                        help="A path to a an audio file with the target speech input for the model.")
+    parser.add_argument("-o", "--output-path", type=str,
+                        default="./out.wav",
+                        help="Output file path.")
+
+    main(parser.parse_args())
