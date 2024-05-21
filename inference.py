d3f57b0b61cab6e2073b79d342c9f44b5322c117
yonicohen <yonathan.cohen1@mail.huji.ac.il>
2024-05-21 17:18:31 +0300

    Added defaults print for help.

--- inference.py
+++ inference.py
@@ -39,13 +39,12 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='StreamVC Inference Script',
-        description='Inference script for StreamVC model, performs voice conversion on a single audio file')
-    parser.add_argument("--betas", type=float, nargs=2, default=(0.5, 0.9))
+        description='Inference script for StreamVC model, performs voice conversion on a single audio file',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("-c", "--checkpoint", type=str,
                         default="./model.safetensors",
                         help="A path the a pretrained StreamVC model checkpoint.")
-
     parser.add_argument("-s", "--source-speech", type=str,
                         help="A path to a an audio file with the source speech input for the model.")
     parser.add_argument("-t", "--target-speech", type=str,
